f15064ee26f53fd10a9280dc14ed4e645f1690ce
Jesuslxz <lozanoj@nmsu.edu>
2023-08-30 12:18:30 -0600

    Jesus Lozano Resume site first iteration

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+var searchButton = document.getElementById("search1");
+console.log('start')
+
+function searching(searchButton) {
+    alert('the button has been pressed');
+
+}
+function random (){
+    console.log('something')
+}
+
+searchButton.addEventListener( "click", searching);
+ 
